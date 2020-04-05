2140b6d8fb16b392ac4ec1d0980032cc16943782
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-04-05 21:59:07 +0300

    update transaction repository

--- app/src/main/java/com/example/rxjava/domain/interactors/TransactionInteractor.kt
+++ app/src/main/java/com/example/rxjava/domain/interactors/TransactionInteractor.kt
@@ -1,24 +1,30 @@
 package com.example.rxjava.domain.interactors
 
+import android.util.Log
 import com.example.rxjava.domain.model.Transaction
 import com.example.rxjava.domain.repository.CardRepository
 import com.example.rxjava.domain.repository.TransactionRepository
 import com.example.rxjava.observables.Observable
+import com.example.rxjava.operators.doOnNext
 import com.example.rxjava.operators.flatMap
-import com.example.rxjava.operators.mergeList
+import com.example.rxjava.operators.subscribeOn
+import com.example.rxjava.shedulers.ThreadScheduler
 
 class TransactionInteractor {
     private val cardRepository = CardRepository()
     private val transactionRepository = TransactionRepository()
 
     fun getAllTransactions(): Observable<Transaction> {
-        return Observable.mergeList(
-            listOf(
-                cardRepository.getMyCards(),
-                cardRepository.getOtherCards()
-            )
-        ).flatMap { card ->
-            transactionRepository.getTransactions(card)
-        }
+        return cardRepository.getMyCards()
+            .subscribeOn(ThreadScheduler())
+            .doOnNext { Log.e(TAG, "card = $it, thread = ${Thread.currentThread()}") }
+            .flatMap { card ->
+                transactionRepository.getTransactions(card).subscribeOn(ThreadScheduler())
+            }
+            .doOnNext { Log.e(TAG, "transaction = $it, thread = ${Thread.currentThread()}") }
+    }
+
+    companion object {
+        private const val TAG = "TransactionInteractor"
     }
 }
