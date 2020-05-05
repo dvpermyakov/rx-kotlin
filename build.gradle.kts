c05ead0c7c8a130c77aeec93e9c01317e531ffcc
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-05-05 20:04:02 +0300

    update gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.3.72"
 }
 
@@ -6,6 +7,26 @@ repositories {
     jcenter()
 }
 
+sourceSets.main {
+    java.srcDirs("src/main/java")
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
+}
+
+group = "com.dvpermyakov"
+version = "0.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repository")
+        }
+    }
 }
