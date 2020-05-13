825cb36308bba42a406a5055325e5ed5cdcc943a
dvpermyakov <dvpermyakov1@gmail.com>
2020-05-14 00:28:11 +0300

    add SubscribeOnObservableTest.kt

---
+++ src/test/java/com/dvpermyakov/rx/operators/utility/SubscribeOnObservableTest.kt
@@ -0,0 +1,103 @@
+package com.dvpermyakov.rx.operators.utility
+
+import com.dvpermyakov.rx.observables.Observable
+import com.dvpermyakov.rx.operators.combining.mergeList
+import com.dvpermyakov.rx.operators.creating.create
+import com.dvpermyakov.rx.operators.filtering.filter
+import com.dvpermyakov.rx.operators.transforming.map
+import com.dvpermyakov.rx.shedulers.ThreadScheduler
+import com.dvpermyakov.rx.utils.TestObserver
+import org.junit.Assert
+import org.junit.Test
+
+class SubscribeOnObservableTest {
+
+    @Test
+    fun subscribeOnOneOperator() {
+        val threadScheduler = ThreadScheduler()
+
+        val observable = Observable.create<Int> { emitter ->
+            Assert.assertEquals(
+                threadScheduler.lastExecutedThread?.name,
+                Thread.currentThread().name
+            )
+            emitter.onComplete()
+        }.subscribeOn(threadScheduler)
+
+        val testObserver = TestObserver<Int>()
+        observable.subscribe(testObserver)
+        testObserver.waitForFinished()
+    }
+
+    @Test
+    fun subscribeFirstOnly() {
+        val threadSchedulerFirst = ThreadScheduler()
+        val threadSchedulerSecond = ThreadScheduler()
+
+        val observable = Observable.create<Int> { emitter ->
+            Assert.assertEquals(
+                threadSchedulerFirst.lastExecutedThread?.name,
+                Thread.currentThread().name
+            )
+            emitter.onComplete()
+        }.subscribeOn(threadSchedulerFirst).subscribeOn(threadSchedulerSecond)
+
+        val testObserver = TestObserver<Int>()
+        observable.subscribe(testObserver)
+        testObserver.waitForFinished()
+    }
+
+    @Test
+    fun subscribeOnMultipleOperators() {
+        val threadScheduler = ThreadScheduler()
+
+        val observable = Observable.create<Int> { emitter ->
+            Assert.assertEquals(
+                threadScheduler.lastExecutedThread?.name,
+                Thread.currentThread().name
+            )
+            emitter.onNext(1)
+            emitter.onComplete()
+        }
+
+        val testObserver = TestObserver<Int>()
+        observable
+            .filter { it > 0 }
+            .map { it * 2 }
+            .subscribeOn(threadScheduler)
+            .subscribe(testObserver)
+        testObserver.waitForFinished()
+    }
+
+    @Test
+    fun subscribeOnMultipleSchedulers() {
+        val threadScheduler1 = ThreadScheduler()
+        val observable1 = Observable.create<Int> { emitter ->
+            Assert.assertEquals(
+                threadScheduler1.lastExecutedThread?.name,
+                Thread.currentThread().name
+            )
+            emitter.onComplete()
+        }.subscribeOn(threadScheduler1)
+
+        val threadScheduler2 = ThreadScheduler()
+        val observable2 = Observable.create<Int> { emitter ->
+            Assert.assertEquals(
+                threadScheduler2.lastExecutedThread?.name,
+                Thread.currentThread().name
+            )
+            emitter.onComplete()
+        }.subscribeOn(threadScheduler2)
+
+
+        val testObserver = TestObserver<Int>()
+        Observable.mergeList(
+            listOf(
+                observable1,
+                observable2
+            )
+        ).subscribe(testObserver)
+        testObserver.waitForFinished()
+    }
+
+}
